e934fa689a7718b28c7b7cac932aee297a287e3c
Hakim Ghods <ghods.hakim@hotmail.com>
2021-01-03 23:17:09 +0330

    Fix

--- src/Portal.Web/Program.cs
+++ src/Portal.Web/Program.cs
@@ -3,18 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Portal.Domain;
+using Portal.Persistence;
 using Serilog;
+using Serilog.Sinks.MSSqlServer;
 
 namespace Portal.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            if (args.Length == 1 && args[0] == "init")
+            {
+
+                using (var scope = host.Services.CreateScope())
+                {
+                    var serviceProvider = scope.ServiceProvider;
+
+                    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                    var ensureCreated = await dbContext.Database.EnsureCreatedAsync();
+
+                    if (ensureCreated)
+                    {
+                        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                        var seedIdentity = new SeedIdentityData(userManager, roleManager);
+                        await seedIdentity.Init();
+                    }
+                }
+
+                Console.Write("Seeding users completed.");
+            }
+            else
+            {
+                host.Run();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -28,8 +61,15 @@ namespace Portal.Web
                         if (webHostBuilderContext.HostingEnvironment.IsProduction())
                         {
                             logger.WriteTo.MSSqlServer(
-                                webHostBuilderContext.Configuration.GetSection("Logging:mssql").Value,
-                                "Logs").MinimumLevel.Error();
+                                webHostBuilderContext
+                                .Configuration
+                                .GetSection("Logging:mssql").Value,
+                                  sinkOptions: new MSSqlServerSinkOptions
+                                  {
+                                      TableName = "Logs",
+                                      AutoCreateSqlTable = true
+                                  }
+                                ).MinimumLevel.Error();
                         }
                         else
                         {
---
+++ src/Portal/Persistence/SeedIdentityData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Portal.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal.Persistence
+{
+    public class SeedIdentityData
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedIdentityData(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task Init()
+        {
+            await SeedRoles();
+            await SeedUsers();
+        }
+
+        private async Task SeedRoles()
+        {
+            await _roleManager.CreateAsync(new IdentityRole("admin"));
+        }
+
+        private async Task SeedUsers()
+        {
+            var user = await _userManager
+                .FindByNameAsync("admin@myapp.com");
+            if (user == null)
+            {
+                var adminUser = new ApplicationUser
+                {
+                    Email = "admin@myapp.com",
+                    UserName = "admin@myapp.com",
+                };
+                await _userManager.CreateAsync(adminUser,"123@Abc");
+
+                await _userManager.AddToRoleAsync(adminUser, "admin");
+            }
+        }
+    }
+}
